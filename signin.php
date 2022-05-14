823bc9796108337c202af2c6aec6c1c92ca999dd
ThaarushiN <95586947+ThaarushiN@users.noreply.github.com>
2022-05-14 23:32:02 +0530

    content 4
    just some file scrambling

--- signin.php
+++ signin.php
@@ -12,23 +12,27 @@
 </head>
 <body>
 	<div class="wrapper">
+
 		<?php
 		include('header.html');
 		?>
 
 		<div class="content">
 			<div class=login>
-				<fieldset style="background-image: url(img/foxiee.jpg);">	
-						<legend>Sign In</legend>
+				<fieldset style="background-image: linear-gradient(rgba(0, 0, 0, 0.527),rgba(0, 0, 0, 0.5)),url(img/foxiee.jpg);">	
+						<h2>Sign In</h2><br>
 						<form method="post" onclick="" action="">
-							<label id="label">Name</label>
+
+							<label id="label">Name</label><br>
 							<input type="text" name="usern" placeholder="Input Username">
 							<br>
-							<label id="label">Password</label>
+
+							<label id="label">Password</label><br>
 							<input type="Password" name="passw" placeholder="Input Password">
 							<br>	
-							<button type="submit	">Sign In</button>
-							<button type="	reset">Clear</button>
+
+							<button type="submit	" class="signbtn">Sign In</button>
+							<button type="	reset" class="signbtn">Clear All</button>
 							<br><p>Forgot your password? Don't worry you can reset it<a href="#"> here!</a>
 							</p>
 						</form>
@@ -37,9 +41,11 @@
 		</div><!-- content -->
 
 		<div class="footer">
+
 			<?php
 			include('footer.html');
 			?>
+			
 		</div>
 
 	</div><!-- wrappper -->
